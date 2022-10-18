7b4c9dddb7576c64f4a3f2bef89a5cfb5a45c708
Mr.Saront0 <oryzavalendio@gmail.com>
2022-10-18 12:36:00 +0700

    Add Praktikum folder

---
+++ Praktikum/variable.java
@@ -0,0 +1,10 @@
+public class variable {
+    public static void main(String[] args) {
+        int nilai_pbo = 90;
+        int nilai_web = 80;
+
+        System.out.println("Variabel yang terdapat dalam program adalah : ");
+        System.out.println("Nilai PBO adalah : " +nilai_pbo);
+        System.out.println("Nilai Web adalah : " +nilai_web);
+    }
+}
